1c47c5c73ae7a447abd7440e7f27f90f8bd28705
Thorstein H. Ensrud <thensrud@hotmail.com>
2020-03-02 22:34:53 +0100

    Make JS for names work, improve style

--- functionality.js
+++ functionality.js
@@ -1,17 +1,28 @@
 
-const saveBtn = document.getElementById("saveBtn");
+const saveNameBtn = document.getElementById("saveNameBtn");
 const clearBtn = document.getElementById("clearBtn");
-const outputDiv = document.getElementById("output_div");
+const nameOutputDiv = document.getElementById("name_output_div");
 
-saveBtn.onclick = function () {
-  let user_input = document.getElementById("user_input").value;
-  window.localStorage.setItem("text", user_input);
-  outputDiv.innerHTML += `<div class="output_card">${window.localStorage.getItem("text")}</div>`;
+saveNameBtn.onclick = function () {
+  const user_input_names = document.getElementById("user_input_names").value;
+  const nameList = JSON.parse(window.localStorage.getItem("nameList")) || [];
+  nameList.push(user_input_names);
+  window.localStorage.setItem("nameList", JSON.stringify(nameList));
+  renderNames();
 
-  document.getElementById("user_input").value = "";
+  document.getElementById("user_input_names").value = "";
 };
+
+function renderNames () {
+  const nameListJSON = window.localStorage.getItem("nameList");
+  const nameList = JSON.parse(nameListJSON) || [];
+  nameOutputDiv.innerHTML = "";
+  for (const user_input_names of nameList) {
+    nameOutputDiv.innerHTML += `<div class="output_card">${user_input_names}</div>`;
+  }
+}
 
 clearBtn.onclick = function () {
   window.localStorage.clear();
-  outputDiv.innerHTML = "";
+  nameOutputDiv.innerHTML = "";
 };
